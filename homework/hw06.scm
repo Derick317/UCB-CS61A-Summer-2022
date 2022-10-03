790e0ac7437a1ce0b83e34eea22227d8535839bb
Derick317 <derick.chen@outlook.com>
2022-10-03 17:52:15 +0800

    Add hw06.scm

---
+++ homework/hw06.scm
@@ -0,0 +1,39 @@
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s)
+  (car (cdr s))
+)
+
+(define (caddr s)
+  (car (cddr s))
+)
+
+(define (interleave lst1 lst2)
+  (cond
+    ((null? lst1) lst2)
+    ((null? lst2) lst1)
+    (else
+      (cons 
+        (car lst1) 
+        (interleave lst2 (cdr lst1))
+      )
+    )
+  )
+)
+
+(define (my-filter pred lst)
+  (cond
+    ((null? lst) lst)
+    ((pred (car lst)) (cons (car lst) (my-filter pred (cdr lst))))
+    (else (my-filter pred (cdr lst)))
+  )
+)
+
+(define (concatenate s)
+  (cond
+    ((null? s) s)
+    (else 
+      (append (car s) (concatenate (cdr s)))
+    )
+  )
+)
